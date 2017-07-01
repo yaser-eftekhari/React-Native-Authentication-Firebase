1c9bb26b15bb57b1e70b6febc6e21db0a394685b
Yaser Eftekhari <yaser.eftekhari@irdeto.com>
2017-07-01 18:44:38 -0400

    added logout state

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyAh5_ryDGq1S1IcbWDQa_FO7rSyr-rXgxk',
@@ -14,13 +16,32 @@ class App extends Component {
       storageBucket: 'authfirebase-6e582.appspot.com',
       messagingSenderId: '493129127436'
     });
+
+    firebase.auth().onAuthStateChanged( user => {
+      if(user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch(this.state.loggedIn) {
+      case true:
+        return <Button>Log out</Button>
+      case false:
+        return <LoginForm />
+      default:
+        return <Spinner size="large" />
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
